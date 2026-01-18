bd035dd99e479ca70f34f525e06bc385b95e3354
Tackx <tack@vxn.dev>
2026-01-18 17:31:12 +0100

    Add raylib

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "c8.h"
+#include "raylib.h"
 
 int main(void)
 {
@@ -16,6 +17,19 @@ int main(void)
     c8->ram[C8_PROGRAM_START_LOCATION] = 250;
 
     printf("first byte of the C8 RAM: %d\n", c8->ram[C8_PROGRAM_START_LOCATION]);
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "raylib basic window");
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("It works!", 20, 20, 20, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
