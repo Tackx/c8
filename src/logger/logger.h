5eb1ff7d4f3853dfb95b3160284d4aa35314771a
Tackx <tack@vxn.dev>
2026-08-08 18:06:55 +0200

    Improve debug logging

--- src/logger/logger.h
+++ src/logger/logger.h
@@ -3,7 +3,15 @@
 #ifdef C8_DEBUG
 #include <stdio.h>
 
-#define C8_LOG(format, ...) fprintf(stderr, format __VA_OPT__(, ) __VA_ARGS__)
+// TODO: Handle NO_COLOR
+
+#define C8_LOG(format, ...)                                                                                                                                    \
+    do                                                                                                                                                         \
+    {                                                                                                                                                          \
+        fprintf(stderr, "\e[0;34m[ c8@%s:%d ]:\e[0m ", __FILE__, __LINE__);                                                                                    \
+        fprintf(stderr, format __VA_OPT__(, ) __VA_ARGS__);                                                                                                    \
+    } while (0)
+
 #else
 #define C8_LOG(format, ...)                                                                                                                                    \
     do                                                                                                                                                         \
