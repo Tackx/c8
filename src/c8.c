ccbb7757c42e4807ac044109521f4721186f4ace
Tackx <tack@vxn.dev>
2026-08-14 23:07:21 +0200

    Add display interrupt quirk

--- src/c8.c
+++ src/c8.c
@@ -72,10 +72,23 @@ int c8_run(int argc, char *argv[])
     {
         // Raylib runs at target 60 fps, so each run of the main loop
         // we should process around 12 instructions to achieve ~700 instructions per second.
+        uint8_t draw_quirk = 0;
         for (int i = 0; i < 12; i++)
         {
-            execute_instruction(&c8, decode_instruction(fetch_instruction(&c8)));
+            C8_INSTRUCTION f = fetch_instruction(&c8);
+            C8_INSTRUCTION_DATA d = decode_instruction(f);
 
+            if (d.type == C8_I_DRAW)
+            {
+                draw_quirk++;
+            }
+
+            execute_instruction(&c8, d);
+
+            if (draw_quirk >= 1)
+            {
+                break;
+            }
             // WaitTime(0.5);
         }
 
