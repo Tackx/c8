ede0921ef64dd6b8be296b1683106c1ca7bf50ea
Tackx <tack@vxn.dev>
2026-08-16 13:20:04 +0200

    Update formatting

--- src/keyboard/keyboard.c
+++ src/keyboard/keyboard.c
@@ -8,7 +8,10 @@ static const int raylib_keys[16] = {
     KEY_ZERO, KEY_ONE, KEY_TWO, KEY_THREE, KEY_FOUR, KEY_FIVE, KEY_SIX, KEY_SEVEN, KEY_EIGHT, KEY_NINE, KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F,
 };
 
-bool c8_is_key_down(unsigned short key) { return IsKeyDown(raylib_keys[key]); }
+bool c8_is_key_down(unsigned short key)
+{
+    return IsKeyDown(raylib_keys[key]);
+}
 
 int8_t c8_get_key()
 {
